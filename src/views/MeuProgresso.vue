91f577e6058b8b06fd510a66b7efc55f40f6afad
Alan Santos <alansods@gmail.com>
2023-07-31 19:33:53 -0300

    modelo progresso

--- src/views/MeuProgresso.vue
+++ src/views/MeuProgresso.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="mb-10">
-    <p>Meu Progresso</p>
-    <p>Meu Progresso</p>
-    <p>Meu Progresso</p>
-    <p>Meu Progresso</p>
-    <p>Meu Progresso</p>
-  </div>
+  <AulaTemplate
+    :titulo="$store.state.modulos[0].titulo"
+    :numero="$store.state.modulos[0].numero"
+  >
+    <Card topico="Meu Progresso">
+      <h3>Módulo 01</h3>
+      <v-progress-linear value="30" height="25" color="success" class="rounded">
+        <strong>30%</strong>
+      </v-progress-linear>
+
+      <h3 class="mt-10">Módulo 02</h3>
+      <v-progress-linear value="20" height="25" color="success" class="rounded">
+        <strong>20%</strong>
+      </v-progress-linear>
+    </Card>
+  </AulaTemplate>
 </template>
 
 <script>
-export default {
+import AulaTemplate from "@/components/AulaTemplate.vue";
+import Card from "@/components/Card.vue";
 
-}
+export default {
+  components: {
+    AulaTemplate,
+    Card,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
