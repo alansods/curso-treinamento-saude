03cc1f3c7e70c45b77733a9b3dc2bc4df7adbd87
Alan Santos <alansods@gmail.com>
2023-08-08 14:49:04 -0300

    changes

--- src/components/BoxIcon.vue
+++ src/components/BoxIcon.vue
@@ -1,52 +1,70 @@
 <template>
-    <v-card class="elevation-0 rounded-lg px-2 py-3" :class="tipoToClass[tipo]">
-      <v-card-text class="box-icon text" :class="{'text-center': !$vuetify.breakpoint.smAndUp}">
-        <v-row align="center" justify="center">
+  <v-card
+    class="elevation-0 rounded-lg px-2 py-3"
+    :class="tipoToClass[tipo]"
+  >
+    <v-card-text
+      class="box-icon text"
+      :class="{ 'text-center': !$vuetify.breakpoint.smAndUp }"
+    >
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="1" class="d-flex justify-center">
+          <!-- <v-icon size="60" color="white">{{ tipoIcon }}</v-icon> -->
+          <lord-icon
+            :src="`https://cdn.lordicon.com/${tipoIcon}.json`"
+            trigger="loop"
+            delay="1500"
+            colors="primary:#fff"
+            style="width: 60px; height: 60px"
+          >
+          </lord-icon>
+        </v-col>
 
-          <v-col cols="12" sm="1" class="d-flex justify-center">
-            <v-icon size="60" color="white">{{ tipoIcon }}</v-icon>
-          </v-col>
-
-          <v-col cols="12" sm="11" class="white--text d-flex justify-center flex-column">
-            <h5 class="text-h5 font-weight-bold mb-2">{{ tipoTitle }}</h5>
-            <slot></slot>
-          </v-col>
-
-        </v-row>
-      </v-card-text>
-    </v-card>
+        <v-col
+          cols="12"
+          sm="11"
+          class="white--text d-flex justify-center flex-column"
+        >
+          <h5 class="text-h5 font-weight-bold mb-2">{{ tipoTitle }}</h5>
+          <slot></slot>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 const tipoToIconAndTitle = {
   objetivos_gerais: {
-    icon: 'mdi-bullseye-arrow',
-    title: 'Objetivos Gerais',
-  },
-  apresentacao: {
-    icon: 'mdi-human-male-board',
-    title: 'Apresentação',
+    icon: "zncllhmn",
+    title: "Objetivos Gerais",
   },
   voce_sabia: {
-    icon: 'mdi-help-circle-outline',
-    title: 'Você Sabia?',
+    icon: "nocvdjmh",
+    title: "Você Sabia?",
   },
   atencao: {
-    icon: 'mdi-alert-octagon-outline',
-    title: 'Atenção!',
+    icon: "vacmyjrh",
+    title: "Atenção!",
   },
   video_aula: {
-    icon: 'mdi-video',
-    title: 'Vídeoaula',
+    icon: "hmkhncjw",
+    title: "Vídeoaula",
   },
   recapitulando: {
-    icon: 'mdi-check-decagram',
-    title: 'Recapitulando',
+    icon: "yqzmiobz",
+    title: "Recapitulando",
   },
 };
 
 export default {
-  props: [ "tipo" ],
+  props: ["tipo"],
+
+  data() {
+    return {
+      hoverIcon: false,
+    };
+  },
 
   components: {},
 
@@ -54,7 +72,6 @@ export default {
     tipoToClass() {
       return {
         objetivos_gerais: "objetivos_gerais",
-        apresentacao: "apresentacao",
         voce_sabia: "voce_sabia",
         atencao: "atencao",
         recapitulando: "recapitulando",
@@ -73,7 +90,6 @@ export default {
 
 <style scoped>
 .box-icon {
-  font-size: 1rem
+  font-size: 1rem;
 }
-
 </style>
